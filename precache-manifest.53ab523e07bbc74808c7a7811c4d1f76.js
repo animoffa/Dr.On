9451a6e5f20f449d287955c78c1d170286b58562
Anastasia <animoffa@mail.ru>
2020-05-07 09:07:54 +0300

    Updates

---
+++ precache-manifest.53ab523e07bbc74808c7a7811c4d1f76.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fdba2fd4586533da11175f17e054d37",
+    "url": "/Dr.On/index.html"
+  },
+  {
+    "revision": "be425762650d2a3fb1d5",
+    "url": "/Dr.On/static/css/3.0ff178b5.chunk.css"
+  },
+  {
+    "revision": "39c504c52f2176531a57",
+    "url": "/Dr.On/static/css/4.908fa835.chunk.css"
+  },
+  {
+    "revision": "041d9854f2e46f081404",
+    "url": "/Dr.On/static/css/main.a794d95a.chunk.css"
+  },
+  {
+    "revision": "7829cf3e0f7d3ce3c98d",
+    "url": "/Dr.On/static/js/2.7ffc43a3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Dr.On/static/js/2.7ffc43a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be425762650d2a3fb1d5",
+    "url": "/Dr.On/static/js/3.3d54592c.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/Dr.On/static/js/3.3d54592c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39c504c52f2176531a57",
+    "url": "/Dr.On/static/js/4.2bda64e2.chunk.js"
+  },
+  {
+    "revision": "041d9854f2e46f081404",
+    "url": "/Dr.On/static/js/main.bf7a1cb7.chunk.js"
+  },
+  {
+    "revision": "7f4d8a81f96fd67d2d53",
+    "url": "/Dr.On/static/js/runtime-main.506d7692.js"
+  },
+  {
+    "revision": "ea744a809e99921ba380144f59687501",
+    "url": "/Dr.On/static/media/ext.ea744a80.png"
+  },
+  {
+    "revision": "eea90af4f9349881db724cf4b1b0949b",
+    "url": "/Dr.On/static/media/follow.eea90af4.png"
+  },
+  {
+    "revision": "e3b3d379a6cdc00344c03bdc03617bb8",
+    "url": "/Dr.On/static/media/groups.e3b3d379.png"
+  },
+  {
+    "revision": "74e518a10484e3d739c36516534f6986",
+    "url": "/Dr.On/static/media/heart.74e518a1.png"
+  },
+  {
+    "revision": "580a2b0abdd8ec37bfb4e2d47d491250",
+    "url": "/Dr.On/static/media/message.580a2b0a.png"
+  },
+  {
+    "revision": "68d58cbd8c803670d3ebb7ffe9012423",
+    "url": "/Dr.On/static/media/photos.68d58cbd.png"
+  },
+  {
+    "revision": "192fe8e47fe75a8e6063f5ba159948ce",
+    "url": "/Dr.On/static/media/spinner.192fe8e4.gif"
+  },
+  {
+    "revision": "30cec5de1d3fd3fe8aef0b0b206fb134",
+    "url": "/Dr.On/static/media/unfollow.30cec5de.png"
+  },
+  {
+    "revision": "04c38d2c7335c02edd90b2ac459aa21f",
+    "url": "/Dr.On/static/media/unnamed.04c38d2c.png"
+  },
+  {
+    "revision": "4e564bbd6896c4f92bbe92b9ba38b4da",
+    "url": "/Dr.On/static/media/unnamed2.4e564bbd.png"
+  }
+]);
